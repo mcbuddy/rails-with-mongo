4c8f433d4751534af7c731e37a8095709fb6c1f5
Budi Sugianto <budisugianto777@gmail.com>
2013-07-23 09:54:43 -0400

    Initial commit for training with mongoDB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWithMongo::Application.config.secret_token = '901aa14aab6f76980bf938ec47d421da8f60c5facc86c58b2107cfd6b6d51b84a29197780943d11129ab630066e1ea2c79df7fc9737dd7e1f91382f30753c63a'
