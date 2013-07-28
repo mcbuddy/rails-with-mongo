1161a2e7ebe14abdd5bac41aabc08f9e60289602
Budi Sugianto <budisugianto777@gmail.com>
2013-07-28 14:50:50 -0400

    Set the config for testing

--- config/application.rb
+++ config/application.rb
@@ -59,10 +59,21 @@ module RailsWithMongo
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+    # Required for heroku
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+    end
+
+    config.filter_parameters += [:password, :password_confirmation]
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsWithMongo::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
